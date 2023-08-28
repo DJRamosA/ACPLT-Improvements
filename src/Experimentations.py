30c38824d70dd9efbd9e1bfefecc133e35d749ec
Diego <diegor98@gmail.com>
2023-08-28 19:19:28 -0400

    Autosave and results from decision trees, svc poly and rbf

--- src/Experimentations.py
+++ src/Experimentations.py
@@ -3,6 +3,8 @@ import pandas as pd
 from sklearn.model_selection import KFold
 from sklearn.model_selection import StratifiedKFold
 import itertools
+import csv
+import os
 from sklearn.metrics import accuracy_score
 
 class ParamSearch:
@@ -16,6 +18,8 @@ class ParamSearch:
         self.n_folds = n_folds
         self.CV = StratifiedKFold(n_splits = n_folds)
 
+        self.fields = ["params", "train_mean", "train_std", "test_mean", "test_std"]
+
         self.cv_results_ = {
             "params": self.param_grid,
             "train_mean": np.zeros(1),
@@ -25,13 +29,21 @@ class ParamSearch:
         }
         
 
-    def fit(self, X:np.ndarray, y:np.ndarray):
+    def fit(self, X:np.ndarray, y:np.ndarray, name_file:str):
         
         
         mean_acc_test = np.zeros(len(self.param_grid))
         std_acc_test = np.zeros(len(self.param_grid))
         mean_acc_train = np.zeros(len(self.param_grid))
         std_acc_train = np.zeros(len(self.param_grid))
+
+        # TODO: aggregate an autosave
+
+        if not os.path.isfile(name_file):
+            with open(name_file, 'w', newline='') as f:
+                csvwriter = csv.DictWriter(f, fieldnames = self.fields)
+                csvwriter.writeheader() 
+                f.close()
 
         for i, d in enumerate(self.param_grid):
 
@@ -61,6 +73,20 @@ class ParamSearch:
             std_acc_test[i] = temp_test_acc.std()
             mean_acc_train[i] = temp_train_acc.mean()
             std_acc_train[i] = temp_train_acc.std()
+        
+            row=[
+                d,
+                mean_acc_train.mean(),
+                std_acc_train.std(),
+                mean_acc_test.mean(),
+                std_acc_test.std()
+            ]
+            
+            with open(name_file, 'a', newline='') as f:
+                csvwriter = csv.writer(f)
+                csvwriter.writerow(row)
+                f.close()
+
 
         self.cv_results_["train_mean"]=mean_acc_train
         self.cv_results_["train_std"]=std_acc_train
@@ -70,6 +96,7 @@ class ParamSearch:
         return self
     
 
+    # ! Delete this function
     def to_csv(self, name_file:str, path:str = ''):
         
         params_values = np.array([list(d.values())[0] for d in self.cv_results_["params"]])
@@ -93,9 +120,8 @@ if __name__ == "__main__":
     proc = GaussianNB()
     iris = datasets.load_iris()
     
-    parameters = {'var_smoothing': (1e-9, 1)}
+    parameters = {'var_smoothing': (1e-9, 1, 100)}
 
     search = ParamSearch(model=proc, param_grid=parameters, n_folds=5)
-    search.fit(iris.data, iris.target)
-    search.to_csv(r'C:/Users/dra98/OneDrive/Documentos/Trabajo/Doctorado/Codigo/src/tests/data.csv')
+    search.fit(iris.data, iris.target,r'C:/Users/dra98/OneDrive/Documentos/Trabajo/Doctorado/Codigo/src/tests/data.csv')
     print(search.cv_results_)
